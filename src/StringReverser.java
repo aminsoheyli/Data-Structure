34d04dac3da9827da58e334a131054343db59510
Amin Soheyli <aminsoheyli98@gmail.com>
2020-11-04 02:01:46 +0330

    Add StringReverer

---
+++ src/StringReverser.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class StringReverser {
+    public static String reverse(String input) {
+        if (input == null)
+            throw new IllegalArgumentException();
+
+        var stack = new Stack<Character>();
+        for (char ch : input.toCharArray())
+            stack.push(ch);
+
+        var reversed = new StringBuilder();
+        while (!stack.isEmpty())
+            reversed.append(stack.pop());
+
+        return reversed.toString();
+    }
+}
