f68c5d11704572140d594ff0617f59de413ced6f
Amin Soheyli <aminsoheyli98@gmail.com>
2020-11-04 18:35:54 +0330

    Add ArrayQueue

---
+++ src/ArrayQueue.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class ArrayQueue {
+    private int[] items;
+    private int front;
+    private int rear;
+    private int count;
+
+    public ArrayQueue(int capacity) {
+        items = new int[capacity];
+        front = rear = 0;
+    }
+
+    public void enqueue(int item) {
+        if (count == items.length)
+            throw new IllegalStateException();
+        items[rear] = item;
+        rear = (rear + 1) % items.length;
+        count++;
+    }
+
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException();
+        var item = items[front];
+        items[front] = 0;
+        front = (front + 1) % items.length;
+        count--;
+        return item;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException();
+        return items[front];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public boolean isFull() {
+        return count == items.length;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
+}
