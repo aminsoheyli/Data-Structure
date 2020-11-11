9be1a78d771ee338efebb3bb1a2535b590eed458
Amin Soheyli <aminsoheyli98@gmail.com>
2020-11-11 23:26:23 +0330

    Change MaxHeap's constructor access level from public to private - Add Heap class     - Add createMaxHeap method to create a MaxHeap

--- src/non_linear/Heap.java
+++ src/non_linear/Heap.java
@@ -7,4 +7,35 @@ public class Heap {
     public static MaxHeap createMaxHeap(int size) {
         return new MaxHeap(size);
     }
+
+    public static void heapify(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            heapify(array, i);
+    }
+
+    private static void heapify(int[] array, int index) {
+        var largerIndex = index;
+
+        var leftIndex = index * 2 + 1;
+        if (leftIndex < array.length &&
+                array[leftIndex] > array[largerIndex])
+            largerIndex = leftIndex;
+
+        var rightIndex = index * 2 + 2;
+        if (rightIndex < array.length &&
+                array[rightIndex] > array[largerIndex])
+            largerIndex = rightIndex;
+
+        if (index == largerIndex)
+            return;
+
+        swap(array, index, largerIndex);
+        heapify(array, largerIndex);
+    }
+
+    private static void swap(int[] array, int first, int second) {
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
 }
